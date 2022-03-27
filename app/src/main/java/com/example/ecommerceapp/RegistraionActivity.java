2398a4fdb1d48aa2a48f8084a54954eb0e033747
Administrator <quang.197pm31195@vanlanguni.vn>
2022-03-27 15:55:40 +0700

    Pass sline sign up

--- app/src/main/java/com/example/ecommerceapp/RegistraionActivity.java
+++ app/src/main/java/com/example/ecommerceapp/RegistraionActivity.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistraionActivity extends AppCompatActivity {
+
+    EditText name,email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registraion);
+
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
     }
 
     public void signin(View view) {
@@ -19,6 +28,26 @@ public class RegistraionActivity extends AppCompatActivity {
     }
 
     public void signup(View view) {
+        String userName = name.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if (TextUtils.isEmpty(userName)){
+            Toast.makeText(this,"Enter Name!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this,"Enter Email Address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(userPassword)){
+            Toast.makeText(this,"Enter Password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (userPassword.length() < 6){
+            Toast.makeText(this,"Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+            return;
+        }
         startActivity(new Intent(RegistraionActivity.this,MainActivity.class));
     }
 }
